0b536ad97a227cfbb05359045774aa587f37eec8
Cristi Constantin <cristi.constantin@live.com>
2018-01-11 14:24:45 +0200

    feat(portScan): scan a range of ports from an IP or HOST

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const net = require('net')
 const DEFAULT_TIMEOUT = 2500
 
 function validateHost (host) {
+  if (!host) {
+    return
+  }
   if (host.trim().length > 0) {
     return true
   }
 }
 
-async function portCheck (
-  host,
-  options = { port: 80, timeout: DEFAULT_TIMEOUT }
-) {
+async function portCheck (host, options = {}) {
   /*
    * Check a specific port from an IP, or a HOST.
    * The `host` must be an IP, or a HOST (defaults to localhost).
@@ -19,15 +19,21 @@ async function portCheck (
    * If the port is open, returns the connection time, in miliseconds.
    * If the host doesn't exist, or the port is closed, returns -1.
    */
+  const config = { port: 80, timeout: DEFAULT_TIMEOUT }
 
-  if (!options.host) {
+  if (typeof host === 'object') {
+    options = { ...config, ...host }
+    host = options.host
+  } else {
+    options = { ...config, ...options }
+  }
+
+  if (!options.host && typeof host === 'string') {
     options.host = host
   }
+
   if (!validateHost(options.host)) {
     throw new Error('host cannot be empty')
-  }
-  if (!options.timeout) {
-    options.timeout = DEFAULT_TIMEOUT
   }
 
   return new Promise(function (resolve) {
@@ -60,6 +66,50 @@ async function portCheck (
   })
 }
 
+async function portScan (host, options = {}) {
+  /*
+   * Check a range of ports from an IP, or a HOST.
+   * The `host` must be an IP, or a HOST (defaults to localhost).
+   * The `startPort` and `endPort` must be a numbers between 1 and 65,535.
+   * If the port is open, returns the connection time, in miliseconds.
+   * If the host doesn't exist, or the port is closed, returns -1.
+   */
+  const config = { startPort: 80, endPort: 443, timeout: DEFAULT_TIMEOUT }
+
+  if (typeof host === 'object') {
+    options = { ...config, ...host }
+    host = options.host
+  } else {
+    options = { ...config, ...options }
+  }
+
+  if (!options.host && typeof host === 'string') {
+    options.host = host
+  }
+  if (!validateHost(options.host)) {
+    throw new Error('host cannot be empty')
+  }
+
+  if (options.startPort < 1 || options.startPort > 65535) {
+    throw new Error('startPort option out of range 1...65,535')
+  }
+  if (options.endPort < 1 || options.endPort > 65535) {
+    throw new Error('endPort option out of range 1...65,535')
+  }
+
+  const openPorts = []
+
+  for (let port = options.startPort; port <= options.endPort; port++) {
+    const success = await portCheck({ host, port, timeout: options.timeout })
+    if (success > 0) {
+      openPorts.push(port)
+    }
+  }
+
+  return openPorts
+}
+
 module.exports = {
-  portCheck
+  portCheck,
+  portScan
 }
--- test.js
+++ test.js
@@ -1,10 +1,29 @@
 import test from 'ava'
-import { portCheck } from './'
+import { portCheck, portScan } from './'
 
-test('test google.com', async t => {
+test('test google.com default port', async t => {
   // Would be funny to fail :))
-  let time = await portCheck('google.com')
+  const time = await portCheck('google.com')
   t.true(time > 0)
-  time = await portCheck('google.com', { port: 443 })
+})
+
+test('test google.com:443', async t => {
+  // Would be funny to fail :))
+  const time = await portCheck('google.com', { port: 443 })
   t.true(time > 0)
+})
+
+test('test google.com port scanning', async t => {
+  const ports = await portScan('google.com', { startPort: 80, endPort: 84, timeout: 250 })
+  t.deepEqual(ports, [80])
+})
+
+test('test port scanning end-port', async t => {
+  const ports = await portScan({ host: 'google.com', endPort: 84, timeout: 250 })
+  t.deepEqual(ports, [80])
+})
+
+test('test port scanning start-port', async t => {
+  const ports = await portScan({ host: 'google.com', startPort: 440, timeout: 250 })
+  t.deepEqual(ports, [443])
 })
